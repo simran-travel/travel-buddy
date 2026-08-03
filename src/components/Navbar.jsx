3afcfc7d23a906d1959fdd3868c31de4e87f0c6e
Simran Khutale <simrankhutale.sk@gmail.com>
2026-08-03 15:51:23 +0530

    Travel Buddy v1.1 - Responsive UI polish

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,34 +3,51 @@ import { useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className="navbar">
       <div className="logo">
         🌍 <span>Travel Buddy</span>
       </div>
 
-      <ul className="nav-links">
+      <button
+  className="menu-toggle"
+  onClick={() => setMenuOpen(!menuOpen)}
+>
+  ☰
+</button>
+
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
         <li>
-  <NavLink to="/" end>
-    Home
+  <NavLink to="/" end onClick={() => setMenuOpen(false)}>
+  Home
+</NavLink>
+</li>
+
+        <li>
+  <NavLink to="/wishlist" onClick={() => setMenuOpen(false)}>
+    ❤️ Wishlist
   </NavLink>
 </li>
 
         <li>
-          <NavLink to="/wishlist">❤️ Wishlist</NavLink>
-        </li>
+  <NavLink to="/favorites" onClick={() => setMenuOpen(false)}>
+    ❤️ Favorites
+  </NavLink>
+</li>
 
         <li>
-          <NavLink to="/favorites">❤️ Favorites</NavLink>
-        </li>
+  <NavLink to="/my-trips" onClick={() => setMenuOpen(false)}>
+    🧳 My Trips
+  </NavLink>
+</li>
 
         <li>
-          <NavLink to="/my-trips">🧳 My Trips</NavLink>
-        </li>
+  <NavLink to="/add-trip" onClick={() => setMenuOpen(false)}>
+    ➕ Add Trip
+  </NavLink>
+</li>
 
-        <li>
-          <NavLink to="/add-trip">➕ Add Trip</NavLink>
-        </li>
       </ul>
     </nav>
   );
